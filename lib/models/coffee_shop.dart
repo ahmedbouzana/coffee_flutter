95d853bf31502ce50684a584df58d5c52524d112
ahmedbouzana <ah.bouzana@gmail.com>
2024-05-22 14:26:52 +0100

    ListView

---
+++ lib/models/coffee_shop.dart
@@ -0,0 +1,38 @@
+import 'package:coffeetute/models/coffee.dart';
+import 'package:flutter/material.dart';
+
+class CoffeeShop extends ChangeNotifier {
+  //coffee for sale list
+  final List<Coffee> _shop = [
+    Coffee(
+        name: 'Long Black', price: "4.10", imagePath: "lib/images/black.png"),
+    Coffee(name: 'Late', price: "4.20", imagePath: "lib/images/latte.png"),
+    Coffee(
+        name: 'Espresso', price: "3.50", imagePath: "lib/images/espresso.png"),
+    Coffee(
+        name: 'Iced Coffee',
+        price: "4.40",
+        imagePath: "lib/images/iced_coffee.png")
+  ];
+
+  //user cart
+  List<Coffee> _userCart = [];
+
+  //get coffee list
+  List<Coffee> get coffeeShop => _shop;
+
+  //get user cart
+  List<Coffee> get usercart => _userCart;
+
+  //add item to cart
+  void addItemToCart(Coffee coffee) {
+    _userCart.add(coffee);
+    notifyListeners();
+  }
+
+  //remove item from cart
+  void removeItemFromCart(Coffee coffee) {
+    _userCart.remove(coffee);
+    notifyListeners();
+  }
+}
--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,7 @@
+import 'package:coffeetute/models/coffee.dart';
+import 'package:coffeetute/models/coffee_shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,8 +13,36 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('SHOP'),
+    return Consumer<CoffeeShop>(
+      builder: (context, value, child) => SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children: [
+              //heading message
+              const Text(
+                'How would you like your coffee?',
+                style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 25),
+
+              //list of coffe to buy
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: value.coffeeShop.length,
+                      itemBuilder: (context, index) {
+                        //get individual coffee
+                        Coffee eachCoffee = value.coffeeShop[index];
+
+                        //return the tile for this coffee
+                        return ListTile(
+                          title: Text(eachCoffee.name),
+                        );
+                      }))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
